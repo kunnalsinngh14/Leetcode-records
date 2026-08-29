6acf8c689fd0bda65e214c9893c637a3ff328462
Kunal Singh <kunal14072005@gmail.com>
2026-08-29 22:13:09 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,8 +1,43 @@
 class Solution {
+    public int bsearch(int[]nums, int target, int left, int right){
+        while(left<=right){
+            int mid = left + (right-left) / 2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return -1;
+    }
+   
     public int search(int[] nums, int target) {
-        for(int i  = 0; i<nums.length; i++){
-            if(nums[i]==target){
-                return i;
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right-left) / 2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[left]){
+                if(target<=nums[mid] && nums[left]<=target){
+                    int ans = bsearch(nums,target,left,mid);
+                    return ans;
+                }
+                left = mid+1;
+            }
+            else{
+                if(nums[mid]<nums[right]){
+                    if(target<=nums[right] && target>nums[mid]){
+                        int ans = bsearch(nums,target,mid,right);
+                        return ans;  
+                    }
+                }
+                right = mid - 1;
             }
         }
         return -1;
