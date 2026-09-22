ff23b4af649872c540e5675a712acdcb5569e796
Kunal Singh <kunal14072005@gmail.com>
2026-09-22 19:55:23 +0530

    Time: 0 ms (100%), Space: 43.9 MB (15.24%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,38 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    List<Integer> arr = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        if (root == null) {
-            return ans;
+        striver(root,0);
+        return arr;
+    }
+    public void striver(TreeNode root, int level){
+        if(root==null){
+            return;
         }
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode curr = q.poll();
-
-                // Last node of this level
-                if (i == size - 1) {
-                    ans.add(curr.val);
-                }
-
-                if (curr.left != null) {
-                    q.offer(curr.left);
-                }
-
-                if (curr.right != null) {
-                    q.offer(curr.right);
-                }
-            }
+        else if(arr.size()==level){
+            arr.add(root.val);
         }
-
-        return ans;
+        striver(root.right, level+1);
+        striver(root.left, level+1);
     }
 }
